54728a31af4ab24cdbc3d772c45e17de26577610
abil <nbil8429@gmail.com>
2022-11-09 09:24:22 +0700

    testtbill

---
+++ abil -oop-8sep.dart
@@ -0,0 +1,11 @@
+class DataKaryawan {
+  void nama():
+  void jabatan():
+}
+
+class DataHasta extends DataKaryawan{
+    void nama()
+}
+  print ('HALOO NAMA SAYA $nama JABATAN SAYA $jabatan');
+
+  
--- abil-oop-principle.dart
+++ abil-oop-principle.dart
@@ -28,3 +28,91 @@ void main() {
     print("id: ${emp._id}");
     print("nama: ${emp._nama}");
   }
+
+  \\\abstrak
+  abstract class abil {
+    // Creating Abstract Methods
+    void katakan();
+    void menulis();
+}
+ \\ inheritance
+class abilforabil extends abil{ 
+    void katakan()
+    {
+        print("abil");
+    }
+ 
+    @override
+    void menulis()
+    {
+        print("HANOMAN");
+    }
+}
+ 
+main()
+{
+    abilforabil geek = new abilforabil();
+    geek.katakan();
+    geek.menulis();
+}
+
+\\\
+ class abil {
+    // Creating Abstract Methods
+    void katakan();
+    void menulis();
+}
+ \\ inheritance
+class abilforabil extends abil{ 
+    void katakan()
+    {
+        print("abil");
+    }
+ 
+    @override
+    void menulis()
+    {
+        print("HANOMAN");
+    }
+}
+ 
+main()
+{
+    abilforabil geek = new abilforabil();
+    geek.katakan();
+    geek.menulis();
+}
+\\Polymorphism
+class Abil {
+    String katakan() {
+      return 'hello';
+    }
+    void menulis() {
+      print ('99');
+    }
+}
+
+class AbilForAbil extends Abil{ 
+    void katakann()
+    {
+        print("abil");
+    }
+/// pholimysum
+    @override
+    void menulis() {
+    
+        print(55);
+        
+    }
+}
+ 
+main()
+{
+    var abil = AbilForAbil();
+    abil.katakann();
+    abil.menulis();
+  
+}
+
+
+ 
